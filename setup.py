627222f9d8b130bc91cb1000870b86baf37d59ed
Miguel Ferrer <ingferrermiguel@gmail.com>
2018-02-26 09:14:49 -0500

    Add MANIFEST.in; Bump ver. 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='mercadolibre-python',
-      version='0.2',
+      version='0.2.1',
       description='API wrapper for MercadoLibre written in Python',
       long_description=read('README.md'),
       url='https://github.com/GearPlug/mercadolibre-python',
